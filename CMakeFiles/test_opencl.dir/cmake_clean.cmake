3e957187f8b44d9ff0328bb60aa94d7942ae8ca7
martluide <martluide@hotmail.com>
2015-02-27 14:07:55 +0000

    Revert "Revert "upload2""
    This reverts commit de42bbd08774d089fdb27e13f3d619efb68454a7.

---
+++ CMakeFiles/test_opencl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_opencl.dir/src/heat.cpp.o"
+  "CMakeFiles/test_opencl.dir/src/test_opencl.cpp.o"
+  "test_opencl.pdb"
+  "test_opencl.exe"
+  "libtest_opencl.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_opencl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
